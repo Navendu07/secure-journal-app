9e586c216604c64d5a474e067c123fdb7d36006d
Navendu07 <166049418+Navendu07@users.noreply.github.com>
2026-02-18 15:48:42 +0530

    Create MailConfig.java

---
+++ src/main/java/in/engineeringdigest/journalApp/config/MailConfig.java
@@ -0,0 +1,27 @@
+package in.engineeringdigest.journalApp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfig {
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl sender = new JavaMailSenderImpl();
+        sender.setHost("smtp.gmail.com");
+        sender.setPort(587);
+        sender.setUsername(System.getenv("MAIL_USERNAME"));
+        sender.setPassword(System.getenv("MAIL_PASSWORD"));
+
+        Properties props = sender.getJavaMailProperties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+
+        return sender;
+    }
+}
